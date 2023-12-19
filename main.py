99a364251da690296d491a79f593595b35813e4e
mahdi <mahdiamr83@gmail.com>
2023-12-20 02:37:39 +0330

    for first part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
-
 import json
+import math
 
 
 def tokenizer(text):
@@ -8,28 +7,29 @@ def tokenizer(text):
     return text
 
 
-unique_word = {}
-i = 0
-for document_number in range(0, 50001):
-    with open(f"./data/document_{document_number}.txt", encoding="utf-8") as f:
-        lines = f.readlines()
-        for line in lines:
-            word_counted = set()
-            splited_line = line.split()
-            for word in splited_line:
-                if tokenizer(word) not in unique_word.keys():
-                    unique_word[tokenizer(word)] = {}
-                    unique_word[tokenizer(word)]["index"] = i
-                    unique_word[tokenizer(word)]["paragraph"] = 1
-                    i += 1
-                    word_counted.add(tokenizer(word))
-                elif tokenizer(word) not in word_counted:
-                    unique_word[tokenizer(word)]["paragraph"] += 1
-                    word_counted.add(tokenizer(word))
+# for unique word and df of unique words
+# unique_word = {}
+# i = 0
+# for document_number in range(0, 50001):
+#     with open(f"./data/document_{document_number}.txt", encoding="utf-8") as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             word_counted = set()
+#             splited_line = line.split()
+#             for word in splited_line:
+#                 if tokenizer(word) not in unique_word.keys():
+#                     unique_word[tokenizer(word)] = {}
+#                     unique_word[tokenizer(word)]["index"] = i
+#                     unique_word[tokenizer(word)]["paragraph"] = 1
+#                     i += 1
+#                     word_counted.add(tokenizer(word))
+#                 elif tokenizer(word) not in word_counted:
+#                     unique_word[tokenizer(word)]["paragraph"] += 1
+#                     word_counted.add(tokenizer(word))
 
 # write for unique_words.json
-with open("./unique_words.json", "w") as myfile:
-    json.dump(unique_word, myfile)
+# with open("./unique_words.json", "w") as myfile:
+#     json.dump(unique_word, myfile)
 
 with open("./unique_words.json") as fp:
     unique_words = json.load(fp)
@@ -45,22 +45,33 @@ def Tf(selected_word, paragraph):
     return tf
 
 
-def Df(selected_word):
-    df = 0
-    for document_number in range(0, 50001):
-        with open(f"./data/document_{document_number}.txt", encoding="utf-8") as f:
-            lines = f.readlines()
-            for line in lines:
-                if selected_word in line.lower():
-                    df += 1
-                    break
-    return df
+last_index = list(unique_words.values())[-1]['index']
+paragraph_numbers = 0
+
+# paragraph number
+for document_number in range(0, 50001):
+    with open(f"./data/document_{document_number}.txt", encoding="utf-8") as f:
+        lines = f.readlines()
+        paragraph_numbers += len(lines)
 
 
-m = {1, 2, 3}
-print(1 in m)  # unique_words_df = {}
-# for key in tqdm(list(unique_words.keys())):
-#     unique_words_df[key] = Df(key)
 
-# with open("./unique_words_df.json", "w") as myfile:
-#     json.dump(unique_words_df, myfile)
+
+def Tf_Idf(paragraph):
+    vector = [0] * (last_index + 1)
+    splited_paragraph = paragraph.split()
+    for word in splited_paragraph:
+        tf = Tf(tokenizer(word), paragraph)
+        index = unique_words[tokenizer(word)]['index']
+        df = unique_words[tokenizer(word)]['paragraph']
+        idf = paragraph_numbers / df
+        tf_idf = tf * math.log(idf, 10) / len(splited_paragraph)
+        vector[index] = tf_idf
+    return vector
+
+for document_number in range(0,50001):
+    with open(f"./data/document_{document_number}.txt", encoding="utf-8") as f:
+        lines = f.readlines()
+        with open(f"./Tf_idf's/document_{document_number}.txt",mode='w',  encoding="utf-8") as f1:
+            for paragraph in lines:
+                f1.write(str(Tf_Idf(paragraph))+'\n')
