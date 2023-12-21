76e979e6906180f51d41a16d800b1521b6bee7b8
mahdi <mahdiamr83@gmail.com>
2023-12-21 15:53:22 +0330

    end of phase 1

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import math
 import string
 
 import numpy as np
-# nltk.download()
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
+
 
 # tokenizer
 def tokenizer(text, tokenizer=TweetTokenizer()):
@@ -16,7 +16,8 @@ def tokenizer(text, tokenizer=TweetTokenizer()):
     stopword_list = stopwords.words('english') + punct + ['rt', 'via', '...', '“', '”', '’']
     return [tok.strip("#") for tok in tokens if tok not in stopword_list and not tok.isdigit()]
 
-#find and save unique word into json
+
+# find and save unique word into json
 # df of unique word
 def save_unique_words():
     unique_word = {}
@@ -42,6 +43,7 @@ def save_unique_words():
     with open("./unique_words.json", "w") as myfile:
         json.dump(unique_word, myfile)
 
+
 # tf
 def Tf(selected_word, splited_paragraph, ):
     tf = 0
@@ -50,7 +52,8 @@ def Tf(selected_word, splited_paragraph, ):
             tf += 1
     return tf
 
-#paragraph number
+
+# paragraph number
 def p_number():
     paragraph_numbers = 0
 
@@ -62,7 +65,8 @@ def p_number():
 
     return paragraph_numbers
 
-#save paragraph tokenizer in json
+
+# save paragraph tokenizer in json
 def save_tokenizer():
     for document_number in range(0, 50001):
         d_paragraph_token = {}
@@ -74,6 +78,7 @@ def save_tokenizer():
                 i += 1
             with open(f"./tokenizer/{document_number}.json", "w") as myfile:
                 json.dump(d_paragraph_token, myfile)
+
 
 # tf idf
 def Tf_Idf(splited_paragraph, last_index, unique_words, paragraph_number):
@@ -87,6 +92,7 @@ def Tf_Idf(splited_paragraph, last_index, unique_words, paragraph_number):
         tf_idf = tf * math.log(idf, 10) / len(splited_paragraph)
         vector[index] = tf_idf
     return np.array(vector)
+
 
 # save tf idf of documents
 def save_tf_idf():
@@ -108,6 +114,7 @@ def save_tf_idf():
             with open(f"./Tf_idf's/{document_number}.txt", mode='w', encoding="utf-8") as f1:
                 f1.write(str(document_vector))
 
+
 # cosine similarity
 def cosine_similarity(query_vector, document_vector):
     numerator = np.dot(query_vector, document_vector)
@@ -116,6 +123,15 @@ def cosine_similarity(query_vector, document_vector):
     denominator = query_norm * document_norm
     cosine = numerator / denominator
     return cosine
+
+
+def get_document_tf_idf(document_name):
+    with open(f"./Tf_idf's/{document_name}.txt", encoding='utf-8') as f:
+        line = f.readline()
+        line = line[1:-1]
+        spilited_line = line.split(',')
+    return spilited_line
+
 
 # return best document for query
 def select_document(query, candidate_document):
@@ -129,17 +145,15 @@ def select_document(query, candidate_document):
     query_tf_idf = Tf_Idf(query_tokenized, last_index, unique_words, paragraph_number)
 
     for document_name in candidate_document:
-        with open(f"./Tf_idf's/{document_name}.txt", encoding='utf-8') as f:
-            line = f.readline()
-            line = line[1:-1]
-            splited_line = line.split(',')
-            document_tf_idf = np.array(splited_line, dtype='float64')
-            cosine = cosine_similarity(query_tf_idf, document_tf_idf)
-            cosines.append({'cosine': cosine, 'document': document_name})
+        spilited_line = get_document_tf_idf(document_name)
+        document_tf_idf = np.array(spilited_line, dtype='float64')
+        cosine = cosine_similarity(query_tf_idf, document_tf_idf)
+        cosines.append({'cosine': cosine, 'document': document_name})
     print(cosines)
     return max(cosines, key=lambda x: x['cosine'])
 
-# print best document for query
+
+# print best document for query of data.json
 def print_document(index):
     with open("./data.json") as fp:
         data_form = json.load(fp)
@@ -148,15 +162,20 @@ def print_document(index):
     candidate_document = data_form[index]['candidate_documents_id']
     c = None
     if '.' in query or ',' in query:
+        c = query
+        j = 0
         for (index, i) in enumerate(query):
             if i == '.' or i == ',':
-                if not query[index - 1].isdigit():
-                    a = query[:index + 1]
-                    b = query[index + 1:]
+                if j < len(query) and not query[j - 1].isdigit():
+                    a = c[:j + 1]
+                    b = c[j + 1:]
                     c = a + ' ' + b
+                    j += 1
+            j += 1
     if c:
         query = c
     print(select_document(query, candidate_document))
+
 
 # for mistake spelling
 def correct_spelling(word, word_list):
@@ -166,4 +185,50 @@ def correct_spelling(word, word_list):
     return word
 
 
-# print_document(0)
+# return top 5 most repeated
+def get_most_repeated(document_name, unique_words, paragraph_number):
+    document_tf_idf = get_document_tf_idf(document_name)
+    tfs = []
+    keys = list(unique_words.keys())
+    for (index, word_tf_idf) in enumerate(document_tf_idf):
+        word_tf_idf = float(word_tf_idf)
+        word = keys[index]
+        df = unique_words[word]['paragraph']
+        tf = word_tf_idf / math.log(paragraph_number / df, 10)
+        tfs.append({'word': word, 'tf': tf})
+    sorted_list = sorted(tfs, key=lambda x: x['tf'], reverse=True)
+    return sorted_list[:5]
+
+
+# print top five most repeated
+def print_most_repeated(document_name):
+    paragraph_number = p_number()
+    with open("./unique_words.json") as fp:
+        unique_words = json.load(fp)
+    for most_repeated in get_most_repeated(document_name, unique_words, paragraph_number):
+        print(most_repeated['word'])
+
+
+# return top five most important
+
+def get_most_important(document_name, unique_words):
+    document_tf_idf = get_document_tf_idf(document_name)
+    tf_idfs = []
+    keys = list(unique_words.keys())
+    for (index, word_tf_idf) in enumerate(document_tf_idf):
+        word_tf_idf = float(word_tf_idf)
+        word = keys[index]
+        tf_idfs.append({'word': word, 'tf_idf': word_tf_idf})
+    sorted_list = sorted(tf_idfs, key=lambda x: x['tf_idf'], reverse=True)
+    return sorted_list[:5]
+
+
+# print top five most important
+def print_most_important(document_name):
+    with open("./unique_words.json") as fp:
+        unique_words = json.load(fp)
+    for most_important in get_most_important(document_name, unique_words):
+        print(most_important['word'])
+
+
+print_most_important(0)
